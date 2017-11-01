6d96729cbf6e7074c76fdc4b3f7b70c494881eca
Matt Hensley <matt.hensley777@gmail.com>
2017-10-31 21:20:14 -0500

    this commit sucks

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const data = require('./data');
+const apiKeys = require('./apiKeys');
+
+apiKeys.retrieveKeys();
+
+
+
+
+const searchAttractions = (query) => {
+	console.log("hello", query);
+  data.getParkAttractions(query).then((data) => {
+    data.showResults(data);
+    console.log("searchAttractions", data);
+  }).catch((error) => {
+    console.log("error in searchAttractions", error);
+  });
+};
