5da07df105d6c289e5fffd353600791f6fda0849
Jessica Rodgers <jessica.rodgers11@gmail.com>
2017-11-04 13:21:01 -0500

    printing areas

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -2,18 +2,19 @@
 
 const domString = (themeParkArray) => {
 	let parkString = '';
-	for(let i =0; i <themeParkArray.length; i++){
+	console.log("theme park array",themeParkArray);
+	for (let i= 0; i < themeParkArray.length; i++){
 		parkString += `<div class = "themeBox">`;
-		// parkString += `<div class="col-xs-6 col-sm-4">.col-xs-6 .col-sm-4</div>`;
-		// parkString += `<div class="col-xs-6 col-sm-4">.col-xs-6 .col-sm-4</div>`;
-		parkString += `<h3>${themeParkArray[i].areas.name}</h3>`;
-		parkString += `<p>${themeParkArray[i].areas.description}</p>`;
-		parkString += `<h3>Stix</h3>`;
-		// parkString += `<div class="clearfix visible-xs-block"></div>`;
-		// parkString += `<div class="col-xs-6 col-sm-4">.col-xs-6 .col-sm-4</div>`;
+		parkString += `<div class="col-xs-6 col-sm-4.col-xs-6 .col-sm-4"></div>`;
+		parkString += `<div class="col-xs-6 col-sm-4.col-xs-6 .col-sm-4"></div>`;
+		parkString += `<h1>${themeParkArray[i].name}</h1>`;
+		parkString += `<p>${themeParkArray[i].description}</p>`;
+		//parkString += `<h3>Stix</h3>`;
+		parkString += `<div class="clearfix visible-xs-block"></div>`;
+		parkString += `<div class="col-xs-6 col-sm-4.col-xs-6 .col-sm-4"></div>`;
 		parkString += `</div>`;	
+	
 	}
-
 	printToDom(parkString);
 };
 
