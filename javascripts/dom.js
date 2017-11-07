310e5d4b1e4672dbc6b9fea1031689787389dc57
Jessica Rodgers <jessica.rodgers11@gmail.com>
2017-11-06 18:43:38 -0600

    dom grid

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -5,16 +5,16 @@ const domString = (themeParkArray) => {
 	console.log("theme park array",themeParkArray);
 	for (let i= 0; i < themeParkArray.length; i++){
 		let areaObj = themeParkArray[i];
-		parkString += `<div class="col-md-4">`;
-		//parkString += `<div class="col-md-4"></div>`;
-		//parkString += `<div class="col-md-4"></div>`;
-		//parkString += `<div class="clearfix visible-md-block"></div>`;
-		parkString += `<h1>${areaObj.name}</h1>`;
-		parkString += `<p>${areaObj.description}</p>`;
-		//parkString += `<p>${areaObj.description}</p>`;
-		//parkString += `<div class="col-md-4"></div>`;
+				//parkString += `<div class="row">`;
+				if (i > 6) {
+				parkString += ` <div class="col-md-4 col-md-offset-4 areaCard">`;
+				}
+				parkString += `<div class="col-md-4 areaCard">`;
+				//parkString += `<div class ="thumbnail">`;
+				parkString += `<h1>${areaObj.name}</h1>`;
+				parkString += `<p>${areaObj.description}</p>`;
+		
 
-		//
 		//parkString += ` <div class="col-md-4 col-md-offset-4"></div>`;
 		parkString += `</div>`;	
 	
